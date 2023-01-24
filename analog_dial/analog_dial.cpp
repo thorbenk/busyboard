e00590f58e81cb609b28e16192e119d534c0d095
Thorben Kroeger <dev@thorben.net>
2023-01-24 23:03:32 +0100

    add LEDs of ARGB PC fan

--- analog_dial/analog_dial.cpp
+++ analog_dial/analog_dial.cpp
@@ -7,6 +7,7 @@ extern "C" {
 }
 #include "dfPlayerDriver.h"
 #include <PicoLed.hpp>
+#include <cmath>
 #include <cstdio>
 #include <iostream>
 
@@ -17,14 +18,40 @@ extern "C" {
 #define DIGITS_CLK_PIN 21
 #define DIGITS_DIO_PIN 20
 
-#define LED_PIN 15
-#define LED_LENGTH 10
+#define PHONE_LEDS_DATA_PIN 15
+#define PHONE_LEDS_LENGTH 10
+
+#define FAN_LEDS_DATA_PIN 14
+#define FAN_LEDS_LENGTH 6
 
 #define DFPLAYER_UART 1
 #define DFPLAYER_MINI_RX 8 /* GP 8 - TX (UART 1) */
 #define DFPLAYER_MINI_TX 9 /* GP 9 - RX (UART 1) */
 
 #define SLIDE_POT_GPIO 26
+
+// See
+auto hsv_to_rgb(float H, float S, float V) -> std::tuple<float, float, float> {
+  float h = std::floor(H / 60.f);
+  float f = H / 60.f - h;
+  float p = V * (1 - S);
+  float q = V * (1 - S * f);
+  float t = V * (1 - S * (1 - f));
+
+  if (h == 0 || h == 6)
+    return std::make_tuple(V, t, p);
+  else if (h == 1)
+    return std::make_tuple(q, V, p);
+  else if (h == 2)
+    return std::make_tuple(p, V, t);
+  else if (h == 3)
+    return std::make_tuple(p, q, V);
+  else if (h == 4)
+    return std::make_tuple(t, p, V);
+  else if (h == 5)
+    return std::make_tuple(V, p, q);
+  return std::make_tuple(0, 0, 0);
+}
 
 int main() {
   stdio_init_all();
@@ -56,23 +83,28 @@ int main() {
   TM1637_clear();
   TM1637_set_brightness(3);
 
-  auto ledStrip = PicoLed::addLeds<PicoLed::WS2812B>(
-      pio1, 0, LED_PIN, LED_LENGTH, PicoLed::FORMAT_WGRB);
-  ledStrip.setBrightness(16);
-  ledStrip.clear();
+  auto phone_dial_leds = PicoLed::addLeds<PicoLed::WS2812B>(
+      pio1, 0, PHONE_LEDS_DATA_PIN, PHONE_LEDS_LENGTH, PicoLed::FORMAT_WGRB);
+  phone_dial_leds.setBrightness(16);
+  phone_dial_leds.clear();
+  phone_dial_leds.fill(PicoLed::RGB(255, 0, 0));
+  phone_dial_leds.show();
 
-  ledStrip.fill(PicoLed::RGB(255, 0, 0));
-  ledStrip.show();
+  auto fan_leds = PicoLed::addLeds<PicoLed::WS2812B>(
+      pio1, 1, FAN_LEDS_DATA_PIN, FAN_LEDS_LENGTH, PicoLed::FORMAT_GRB);
+  fan_leds.setBrightness(255);
+  fan_leds.clear();
+  fan_leds.fill(PicoLed::RGB(0, 255, 0));
+  fan_leds.show();
 
   DfPlayerPico<DFPLAYER_UART, DFPLAYER_MINI_TX, DFPLAYER_MINI_RX> dfp;
-
   dfp.reset();
   sleep_ms(2000);
   dfp.specifyVolume(10);
   sleep_ms(200);
 
-  ledStrip.fill(PicoLed::RGB(0, 0, 255));
-  ledStrip.show();
+  phone_dial_leds.fill(PicoLed::RGB(0, 0, 255));
+  phone_dial_leds.show();
 
   int num = -1;
 
@@ -87,9 +119,24 @@ int main() {
 
     if (time_us_32() > 100 * 1000 + T) {
       T = time_us_32();
-      uint16_t result = adc_read();
-      float f = result / 4096.f;
+      uint16_t const result = adc_read();
+      float const f = result / 4096.f;
       printf("slider at %f\n", f);
+
+      float const h = f * 360.f;
+      float const s = 1.f;
+      float const v = 1.f;
+
+      auto const [r, g, b] = hsv_to_rgb(h, s, v);
+
+      uint8_t R = 255 * r;
+      uint8_t G = 255 * g;
+      uint8_t B = 255 * b;
+
+      printf("R=%d, G=%d, B=%d\n", R, G, B);
+
+      fan_leds.fill(PicoLed::RGB(R, G, B));
+      fan_leds.show();
 
       pwm_set_gpio_level(LED_FADER, result);
     }
@@ -141,12 +188,12 @@ int main() {
         sleep_ms(200);
         printf("display %i\n", num);
         for (int i = 0; i < num; ++i) {
-          ledStrip.setPixelColor(i, PicoLed::RGB(0, 255, 0));
+          phone_dial_leds.setPixelColor(i, PicoLed::RGB(0, 255, 0));
         }
         for (int i = num; i < 10; ++i) {
-          ledStrip.setPixelColor(i, PicoLed::RGB(255, 0, 0));
+          phone_dial_leds.setPixelColor(i, PicoLed::RGB(255, 0, 0));
         }
-        ledStrip.show();
+        phone_dial_leds.show();
         display_num = num;
         num = -1;
         last_num_switch = false;
