b7db81b2427afbf7dbcbc5e893c12ff04c5a5c3c
Thorben Kroeger <dev@thorben.net>
2023-02-08 12:06:12 +0100

    arcade button + ws2812b led experiment

---
+++ arcade_rgb_button/arcade_rgb_button.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#include <PicoLed.hpp>
+
+#define LED_PIN 13
+#define LED_LENGTH 1
+
+int main()
+{
+    stdio_init_all();
+
+    auto ledStrip = PicoLed::addLeds<PicoLed::WS2812B>(pio0, 0, LED_PIN, LED_LENGTH, PicoLed::FORMAT_GRB);
+    ledStrip.setBrightness(128);
+    ledStrip.clear();
+
+    ledStrip.fill( PicoLed::RGB(255, 0, 0) );
+    ledStrip.show();
+
+    while (true) {
+      ledStrip.fill( PicoLed::RGB(255, 0, 0) );
+      ledStrip.show();
+      sleep_ms(2000);
+      ledStrip.fill( PicoLed::RGB(0, 255, 0) );
+      ledStrip.show();
+      sleep_ms(2000);
+      ledStrip.fill( PicoLed::RGB(0, 0, 255) );
+      ledStrip.show();
+      sleep_ms(2000);
+    }
+
+    return 0;
+}
--- format.sh
+++ format.sh
@@ -1,3 +1,4 @@
 clang-format -i analog_dial/analog_dial.cpp
 clang-format -i analog_dial/dfPlayerDriver.h
 clang-format -i vid28-05/motor_vid28.cpp
+clang-format -i arcade_rgb_button/arcade_rgb_button.cpp
